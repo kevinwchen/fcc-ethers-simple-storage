c76b49229c1c26c7afb93b3260ad7427ef7a1e61
Kevin <47770053+kevinwchen@users.noreply.github.com>
2022-11-30 10:32:51 +1100

    Add hardhat implementation

--- deploy.js
+++ deploy.js
@@ -3,10 +3,10 @@ const fs = require("fs-extra"); // reads abi and bin files
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
-    "HTTP://172.26.240.1:7545"
+    "http://127.0.0.1:8545/"
   );
   const wallet = new ethers.Wallet(
-    "c91a0f9f281aa39338cecba8ff4758c4c1fb5c84a9268181ea25ca765824721a",
+    "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
     provider
   );
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
@@ -18,7 +18,9 @@ async function main() {
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
   console.log("Deploying, please wait...");
   const contract = await contractFactory.deploy(); // Tell code to stop and wait for code to deploy
-  console.log(contract);
+  const transactionReceipt = await contract.deployTransaction.wait(1); // wait 1 block confirmation
+  console.log("Let's deploy with only transaction data");
+  const tx = {};
 }
 
 main()
